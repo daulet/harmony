14348cfe849ef289b5edfbd193e5f47f69a1e831
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-04-12 20:58:48 -0700

    Add Logger assembly

---
+++ src/Logger/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logger
+{
+    public interface ILogger
+    {
+        void Error(string message);
+
+        void Info(string message);
+
+        void Verbose(string message);
+
+        void Warning(string message);
+    }
+}
---
+++ src/Logger/Provider/ConsoleLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Logger.Provider
+{
+    public class ColorfulConsole : ILogger
+    {
+        public void Error(string message)
+        {
+            WriteWithForeground(message, ConsoleColor.Red);
+        }
+
+        public void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Verbose(string message)
+        {
+#if DEBUG
+            WriteWithForeground(message, ConsoleColor.DarkGray);
+#endif
+        }
+
+        public void Warning(string message)
+        {
+            WriteWithForeground(message, ConsoleColor.Yellow);
+        }
+
+        private static void WriteWithForeground(string message, ConsoleColor foregroundColor)
+        {
+            var currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(message);
+            Console.ForegroundColor = currentColor;
+        }
+    }
+}
