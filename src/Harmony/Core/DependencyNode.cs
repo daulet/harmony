54c486e3b500997f01a2d9b2069cb862471ac991
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-04-12 21:42:33 -0700

    Add dependency tree types

---
+++ src/Harmony/Core/DependencyNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Harmony.Core
+{
+    internal class DependencyNode
+    {
+        private readonly Type _type;
+        private readonly HashSet<DependencyNode> _dependencies;
+
+        public DependencyNode(Type type)
+        {
+            _type = type;
+            _dependencies = new HashSet<DependencyNode>();
+        }
+
+        public void AddDependency(DependencyNode dependency)
+        {
+            _dependencies.Add(dependency);
+        }
+
+        #region Comparison overrides
+
+        public override bool Equals(object obj)
+        {
+            var otherNode = obj as DependencyNode;
+            if (otherNode != null)
+            {
+                return _type == otherNode._type;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return _type.GetHashCode();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Harmony/Core/DependencyTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Harmony.Core
+{
+    internal class DependencyTree
+    {
+        private readonly Dictionary<Type, DependencyNode> _knownNodes;
+
+        public DependencyTree()
+        {
+            _knownNodes = new Dictionary<Type, DependencyNode>();
+        }
+
+        public void AddDependency(Type subjecType, Type dependsOnType)
+        {
+            var subjectNode = GetNode(subjecType);
+            var dependsOnNode = GetNode(dependsOnType);
+
+            subjectNode.AddDependency(dependsOnNode);
+        }
+
+        public void AddNode(Type knownType)
+        {
+            GetNode(knownType);
+        }
+
+        private DependencyNode GetNode(Type type)
+        {
+            if (!_knownNodes.ContainsKey(type))
+            {
+                _knownNodes.Add(type, new DependencyNode(type));
+            }
+            return _knownNodes[type];
+        }
+    }
+}
